dfe787cd793ac9b6bf5b0686aa822a0dc084c9c7
DrozdovRoman <91407365+DrozdovRoman@users.noreply.github.com>
2022-05-19 15:08:56 +0500

    Update main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w; // Инициализация объекта класса MainWindow
     w.show();
     return a.exec();
 }
